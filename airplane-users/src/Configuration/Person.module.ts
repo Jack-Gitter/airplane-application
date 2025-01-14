dc2a62eeff50d0810a43a90c27f2220642ee2ac4
Jack Gitter <jack.gitter@gmail.com>
2025-01-14 16:57:26 -0500

    PLURALS

--- airplane-users/src/Configuration/Person.module.ts
+++ airplane-users/src/Configuration/Person.module.ts
@@ -1,9 +1,11 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { CreatePersonService } from "src/Application/Person/CreatePersonService";
 import { PersonController } from "src/Controller/Person/Person";
+import { Person } from "src/Domain/Person/Person";
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forFeature([Person])],
   controllers: [PersonController],
   providers: [CreatePersonService],
 })

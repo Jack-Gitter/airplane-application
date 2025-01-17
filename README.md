5c5e2ea8c418d4bcede9674113f6b71f94c58f40
Jack Gitter <jack.gitter@gmail.com>
2025-01-17 13:39:19 -0500

    nice

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 testing: 
 todo: 
     implement the rest of the functionality
-        deleting a schedule (eventual consistency to update all related flights via domain event)
         implement adding segments to a flightschedule
         implement removing segments from a flightschedule
         implement CQRS for retrieval of information
--- airplane-reservations/src/Controller/FlightSchedule/FlightSchedule.controller.ts
+++ airplane-reservations/src/Controller/FlightSchedule/FlightSchedule.controller.ts
@@ -1,14 +1,25 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Delete, Post } from "@nestjs/common";
+import { CancelFlightScheduleService } from "src/Application/FlightSchedule/CancelFlightSchedule.service";
 import { CreateFlightScheduleService } from "src/Application/FlightSchedule/CreateSchedule.service";
+import { CancelFlightScheduleDTO } from "./CancelFlightScheduleDTO";
 
 @Controller('flightSchedule')
 export class FlightScheduleController {
 
-    constructor(private createFlightScheduleService: CreateFlightScheduleService) { }
+    constructor(
+        private createFlightScheduleService: CreateFlightScheduleService,
+        private cancelFlightScheduleService: CancelFlightScheduleService
+    
+    ) { }
 
     @Post()
     public async createFlightSchedule() {
         return await this.createFlightScheduleService.createFlightSchedule()
+    }
+    
+    @Delete()
+    public async cancelFlightSchedule(@Body() cancelFlightScheduleDTO: CancelFlightScheduleDTO) {
+        return await this.cancelFlightScheduleService.cancelFlightSchedule(cancelFlightScheduleDTO.scheduleId)
     }
 
 }

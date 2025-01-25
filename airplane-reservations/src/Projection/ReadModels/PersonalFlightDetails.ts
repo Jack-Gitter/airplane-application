13a332d0f313be23a01be1480c7e3e985137d542
Jack Gitter <jack.gitter@gmail.com>
2025-01-25 12:14:31 -0500

    nice

--- airplane-reservations/src/Projection/ReadModels/PersonalFlightDetails.ts
+++ airplane-reservations/src/Projection/ReadModels/PersonalFlightDetails.ts
@@ -1,7 +1,6 @@
 import { SeatPosition } from "src/Domain/Flight/ValueObjects/SeatPosition";
 import { Location } from "src/Domain/FlightSchedule/ValueObjects/Location";
 
-// this class is responsible for transfering data about all of a users flihgts
 export class PersonalFlightDetails {
     constructor(private seatPosition: SeatPosition, private to: Location, private from: Location) {}
 }
